9a32d4b04b2d87ba032b66827304cdf1bc274be2
JFELO88 <108140470+JFELO88@users.noreply.github.com>
2025-05-08 17:24:52 -0500

    interface
    Se agrega la Ui Interface de la vida, mana, y las tres habilidades

---
+++ UI Interface/Assets/Script/BarraMana.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//public class BarraMana : MonoBehaviour
+//{
+//    public Image rellenoBarraMana;
+//    private PlayerController playerController;
+//    private float manaMaxima;
+//    void Start()
+//    {
+//        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+//        manaMaxima = playerController.mana;
+//    }
+
+
+//    void Update()
+//    {
+//        rellenoBarraMana.fillAmount = playerController.mana / manaMaxima;
+//    }
+//}
---
+++ UI Interface/Assets/Script/BarraVida.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//public class barravida : MonoBehaviour
+//{
+//    public Image rellenobarravida;
+//    private playercontroller playercontroller;
+//    private float vidamaxima;
+//    void start()
+//    {
+//        playercontroller = GameObject.Find("player").GetComponent<playercontroller>();
+//        vidamaxima = playercontroller.vida;
+//    }
+
+
+//    void update()
+//    {
+//        rellenobarravida.fillAmount = playercontroller.vida / vidamaxima;
+//    }
+//}
